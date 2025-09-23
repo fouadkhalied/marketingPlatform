f4ef1c655e58937d73928055a6e5b9817d65dda6
fouadkhalied <foukha49@gmail.com>
2025-09-23 17:24:25 +0300

    fix : vercel deployment

--- this.js
+++
@@ -1,8 +0,0 @@
-const jwt = require('jsonwebtoken');
-const token = jwt.sign({
-    userId: '5508b089-62ea-49ec-8151-0f907d3a36a1',
-    email: 'john.doe@example.com',  
-    role: 'advertiser'
-}, 'marketingPlatform', { expiresIn: '24h' });
-
-console.log(token);
