21c4b762b253005f467b1aa51c0ffeccc29d31eb
fouadkhalied <foukha49@gmail.com>
2025-09-27 14:13:18 +0300

    fix(user): fix factory controller

--- src/modules/user/interfaces/factories/user.factories.ts
+++ src/modules/user/interfaces/factories/user.factories.ts
@@ -2,8 +2,9 @@
 import { JwtService } from "../../../../infrastructure/shared/common/auth/module/jwt.module";
 import { EmailService } from "../../../../infrastructure/shared/common/email/module/resend.module";
 import { OTPService } from "../../../../infrastructure/shared/common/otp/module/otp.module";
-import { FacebookAuthService } from "../../application/services/facebook-app.service";
+import { FacebookPageService } from "../../application/services/facebook-app.service";
 import { UserAppService } from "../../application/services/user-app.service";
+import { FacebookPageRepositoryImpl } from "../../infrastructure/repositories/facebook.repository.impl";
 import { UserRepositoryImpl } from "../../infrastructure/repositories/user.repository.impl";
 import { UserController } from "../controllers/user.controller";
 
@@ -18,17 +19,19 @@ export function createUserController(): UserController {
   // Create repository implementation (infrastructure)
   const userRepository = new UserRepositoryImpl();
 
+  const facebookRepository = new FacebookPageRepositoryImpl();
+
   const emailService = new EmailService();
 
   const otpService = new OTPService(emailService);
 
-  const facebookAuthService = new FacebookAuthService()
+  const facebookPageService = new FacebookPageService(facebookRepository)
 
   // Pass the actual environment variable value, not the string
   const jwtService = new JwtService(jwtSecret);
 
   // Pass repository to application service
-  const userService = new UserAppService(userRepository, otpService, jwtService, facebookAuthService);
+  const userService = new UserAppService(userRepository, otpService, jwtService, facebookPageService);
 
   // Pass application service to controller
   const userController = new UserController(userService);
