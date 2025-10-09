3e352d795a2e5f6864886629059f73671ff294a8
fouadkhalied <foukha49@gmail.com>
2025-10-09 17:57:07 +0300

    fix(auth): fix google oauth flow to redirect to homepage

--- src/modules/auth/application/services/auth-app.service.ts
+++ src/modules/auth/application/services/auth-app.service.ts
@@ -100,14 +100,17 @@ export class AuthService {
       // Handle user login/creation
       const user = await this.handleFacebookLogin(userData);
 
-      // res.json(ResponseBuilder.success({
-      //   token : accessToken,
-      //   username : user.data?.username || userData.name,
-      //   role : user.data?.role
-      // }));
+     // Safely encode parameters for URL
+     const params = new URLSearchParams({
+      token: accessToken,
+      username: user.data?.username || userData.name || 'unknown',
+      role: user.data?.role || 'user'
+    });
 
-      res.redirect(`http://localhost:3000/dashboard?token=${accessToken}&username=${user.data?.username}&role=${user.data?.role}`);
-      
+    const redirectUrl = `http://localhost:3000/dashboard?${params.toString()}`;
+    //console.log('9. Redirecting to:', redirectUrl);
+    
+    res.redirect(redirectUrl);
     } catch (error: any) {
       const errorResponse = ErrorBuilder.build(ErrorCode.INTERNAL_SERVER_ERROR, error.response?.data?.error?.message || error.message || "Facebook authentication failed")
       res.status(500).json(
