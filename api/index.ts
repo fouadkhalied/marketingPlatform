578a7d06f13169ba5d1cd033c8ad387f0a7138c2
fouadkhalied <foukha49@gmail.com>
2025-10-09 19:54:53 +0300

    fix(auth): get impression ratio is for user and admin

--- api/index.ts
+++ api/index.ts
@@ -466,7 +466,7 @@ app.delete('/api/users/:id',AuthMiddleware(UserRole.ADMIN),(req,res) => userCont
 app.put('/api/users/promote/:id',AuthMiddleware(UserRole.ADMIN),(req,res) => userController.makeUserAdmin(req,res));
 
 // Get all available impression ratios 
-app.get('/api/users/impression-ratios', AuthMiddleware(UserRole.USER), (req, res) => userController.getAvailableImpressionRatios(req, res));
+app.get('/api/users/impression-ratios', (req, res) => userController.getAvailableImpressionRatios(req, res));
 
 // Update impression ratio (admin only)
 app.put('/api/users/impression-ratios/:id', AuthMiddleware(UserRole.ADMIN), (req, res) => userController.updateImpressionRatio(req, res));
